f0869a2acd6923db4ccea2ba4b6b41189608085b
Aymen <aymenghaloua@gmail.com>
2024-05-25 07:17:18 +0100

    Adding "Task" Model to DB; Optimizing App Folder.

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,4 +1,34 @@
-const PrismaExamplePage = () => {
-  return <div>PrismaExamplePage</div>;
+import prisma from '@/utils/db';
+
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data: {
+      content: 'wake up',
+    },
+  });
+
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return allTasks;
 };
-export default PrismaExamplePage;
+
+const PrismaExample = async () => {
+  const tasks = await prismaHandlers();
+  return (
+    <div>
+      <h1 className="text-7xl">PrismaExample</h1>
+      {tasks.map((task) => {
+        return (
+          <h2 key={task.id} className="text-xl py-2">
+            {task.content}
+          </h2>
+        );
+      })}
+    </div>
+  );
+};
+export default PrismaExample;
